cfe528ee86a468b84ebf2db4a1af61e1b6c06174
unknown <saurabh.tripathi8958@gmail.com>
2026-05-13 23:58:44 +0530

    Basic structure made

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('startup_funding.csv')
+df = pd.read_csv('D:\Startup_dashboard\Data\processed\cleaned_startup_data.csv')
+df_by_investor = pd.read_csv('D:\Startup_dashboard\Data\processed\startup_investor_data.csv')
 
 #Making sidebar
 st.sidebar.title('Indian Startup Funding Analysis')
@@ -11,10 +12,10 @@ option = st.sidebar.selectbox("Select one", ['Overall Analysis','Startup Analysi
 if option ==  'Overall Analysis':
     st.title('Overall Analysis')
 elif option == 'Startup Analysis':
-    st.sidebar.selectbox('Select startup', ['Byjus', 'Ola', 'Flipkart'])
+    st.sidebar.selectbox('Select startup', sorted(df['StartUp'].unique().tolist()))
     st.title('Startup Analysis')
 else:
-    st.sidebar.selectbox('Select Investor', ['Person_1','Person_2','Person_3','Person_4','Person_5'])
+    st.sidebar.selectbox('Select Investor', sorted(df_by_investor['Investors'].unique().tolist()))
     st.title('Investor Analysis')
 
 
