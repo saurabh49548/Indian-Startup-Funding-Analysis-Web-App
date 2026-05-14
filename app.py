be489a361b649cf034171957a7121b694083d45e
unknown <saurabh.tripathi8958@gmail.com>
2026-05-15 00:30:09 +0530

    Remove inconsistenct from Investor Dataset

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ elif option == 'Startup Analysis':
     st.sidebar.selectbox('Select startup', sorted(df['StartUp'].unique().tolist()))
     st.title('Startup Analysis')
 else:
-    st.sidebar.selectbox('Select Investor', sorted(df_by_investor['Investors'].unique().tolist())git remote -v)
+    st.sidebar.selectbox('Select Investor', sorted(df_by_investor['Investors'].unique().tolist()))
     st.title('Investor Analysis')
 
 
